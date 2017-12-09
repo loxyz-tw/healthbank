b103bc5723446f07b10f46a93b2200f767346e96
LoRoy <loxyz.tw@gmail.com>
2017-12-09 15:33:32 +0800

    Update some words, clinic departments

--- app/Http/Controllers/BMIController.php
+++ app/Http/Controllers/BMIController.php
@@ -29,15 +29,17 @@ class BMIController extends Controller
 			
 		}
 		
+		$bmi = $weight / ($height / 100) / ($height / 100);
+		$bmi1ps = number_format($bmi, 1);
    		$bmiCol = BMI::where('height', $height)->first();
    		
-   		$res = '肥胖';
+   		$res = 'BMI有'.$bmi1ps.'，太~太~太肥了吧!昨天的地震是你造成的嗎?'.'\\n我告訴你齁，要吃營養點的食物啦，太輕了風一吹，咻~的你就不見了';
    		if ($weight < $bmiCol->underweight) {
-   			$res = '過輕';
+   			$res = '哎唷~這樣不行捏!BMI只有'.$bmi1ps.'，太輕了啦!'.'\\n我告訴你齁，要吃營養點的食物啦，太輕了風一吹，咻~的你就不見了';
    		} else if ($weight < $bmiCol->normal) {
-   			$res = '正常';
+   			$res = '你全身發出耀眼的光芒，BMI是'.$bmi1ps.'，太完美了'.'\\n繼續維持這令人羨慕的身材吧!';
    		} else if ($weight < $bmiCol->overweight) {
-   			$res = '過胖';
+   		    $res = '你是不是也自己覺得有點胖了!BMI有'.$bmi1ps.'\\n乖一點別再黑白亂吃了好嗎(啾咪~)';
    		} else {
    			
    		}
--- app/Http/Controllers/ClinicController.php
+++ app/Http/Controllers/ClinicController.php
@@ -20,17 +20,18 @@ class ClinicController extends Controller
    		$json = $request;
 		$city = $json['city'];
 		$dist = $json['dist'];
+		$dept = $json['dept'];
 		$clinicName = ($json['clinicName']);
    		
 		if (empty($clinicName)) {
-			$clinics = Clinic::where('city', 'like', $city.$dist.'%')->take(5)->get();
+			$clinics = Clinic::where([['city', 'like', $city.$dist.'%'], ['clinicName', 'like', '%'.$dept.'%']])->take(5)->get();
 		} else {
 			$clinics = Clinic::where('clinicName', 'like', '%'.$clinicName.'%')->take(5)->get();
 		}
    		
    		$subArray = [];
    		foreach ($clinics as $clinic) {
-   			array_push($subArray, Array($clinic->clinicName, $clinic->address, $clinic->tel));
+   		   array_push($subArray, Array($clinic->clinicName, $clinic->address, $clinic->tel));
    		}
    		
    		return $subArray;
